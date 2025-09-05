fdaa34345b336bee6f2fd2b94907c31236e6177c
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2025-09-05 16:09:02 -0300

    Add files via upload

---
+++ translations/README.es.md.m
@@ -0,0 +1,112 @@
+# 👋🏼Hola!
+
+## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b> Acerca de mí
+ </b>
+<ul>
+ <li>🎓 Estudiante de pregrado en sistemas y medios digitales, en la Universidad Federal de Ceará</li>
+ <li>💻 Técnica en informática, del Instituto Federal de Ceará</li>
+ <li>👩🏽‍💻 Me encanta crear sitios web utilizando HTML, CSS y JavaScript.</li>
+ <li>🖼️ ¡Me encanta estudiar nuevos idiomas!</li>
+</ul>
+
+
+
+## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b>Tecnologías </b>
+
+
+
+<img 
+    align="left" 
+    alt="HTML"
+    title="HTML" 
+    width="40px" 
+    style="padding-right: 10px;" 
+    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" 
+/>
+<img 
+    align="left" 
+    alt="CSS" 
+    title="CSS"
+    width="40px" 
+    style="padding-right: 10px;" 
+    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" 
+/>
+<img 
+    align="left" 
+    alt="JavaScript" 
+    title="JavaScript"
+    width="40px" 
+    style="padding-right: 10px;" 
+    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" 
+/>
+
+<img 
+    align="left" 
+    alt="Git" 
+    title="Git"
+    width="40px" 
+    style="padding-right: 10px;" 
+    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" 
+/>
+
+<img 
+    align="left" 
+    alt="Bootstrap" 
+    title="Bootstrap"
+    width="40px" 
+    style="padding-right: 10px;" 
+    src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" 
+/>
+
+<br>
+<br>
+
+ ## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b>formación profesional </b>
+A través de mi carrera de técnico en informática, he tomado varias clases técnicas (además de las de secundaria regular), tales como:
+
+- Programación web
+- Programación orientada a objetos
+- Programación de dispositivos móviles
+- Fundamentos de programación
+- Bases de datos
+- Ciberseguridad
+- Informática web
+- Administración de sistemas operativos
+- Métodos y herramientas de desarrollo de software
+- Electrónica digital
+- Prácticas profesionales
+  
+ 
+Tomar todas estas clases fortalece mis conceptos sobre el trabajo en equipo, la paciencia, la gestión del tiempo y la organización.
+
+## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b>Háblame </b>
+
+[![Instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://instagram.com/julia_maria0764)
+[![Email](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:julia.maria07@alu.ufc.br)
+
+
+ ## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="35"><b>Estado </b>
+
+<div 
+ style="
+ width:100%;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ "
+ >
+  <img 
+    align="left" 
+    alt="GitHub Stats" 
+    height="200" 
+    style="padding-right: 10px;" 
+    src="https://github-readme-stats.vercel.app/api?username=Julia-maria56&show_icons=true&theme=tokyonight&include_all_commits=true&locale=pt-br" 
+  />
+
+<img 
+      align="right" 
+      alt="GitHub Stats" 
+      height="200" 
+      src="https://github-readme-stats.vercel.app/api/top-langs/?username=Julia-maria56&theme=tokyonight&layout=compact&custom_title=Tecnologias&langs_count=9" 
+  />
+</div>
